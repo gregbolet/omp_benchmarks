0c4a50c9b98bd9973e9ac8c2df0233a4d47242b7
gregboleet <gbolet@vt.edu>
2023-06-07 16:52:34 -0700

    starting new testing scripts

--- exploreHyperparams/benchmarks.py
+++ exploreHyperparams/benchmarks.py
@@ -1,20 +1,26 @@
-
-ROOT_DIR = '/g/g15/bolet1/workspace/ruby-benchmarks'
-
+ROOT_DIR = '/g/g15/bolet1/workspace/lassen-benchmarks'
 
 machines = {
     'ruby' : {
-        'OMP_NUM_THREADS': [4,8,14,28,42,56,70,112],
-        'OMP_PROC_BIND': ['close', 'spread'],
-        'OMP_SCHEDULE_sched': ['static', 'guided', 'dynamic'],
-        'OMP_SCHEDULE_chunk': [1,8,32,64,128,256,512]
+        'envvars': {
+            'OMP_NUM_THREADS': [str(a) for a in [4,8,14,28,42,56,70,112]],
+            'OMP_PROC_BIND': ['close', 'spread'],
+            'OMP_SCHEDULE': [ a+','+str(b) for a in ['static', 'guided', 'dynamic'] for b in [1,8,32,64,128,256,512] ],
+        },
+        'jobsystem' : {
+            'joblaunch' : ''
+        }
     },
 
     'lassen' : {
-        'OMP_NUM_THREADS': [10,20,40,60,80,100,120,140,160],
-        'OMP_PROC_BIND': ['close', 'spread'],
-        'OMP_SCHEDULE_sched': ['static', 'guided', 'dynamic'],
-        'OMP_SCHEDULE_chunk': [1,8,32,64,128,256,512]
+        'envvars': {
+            'OMP_NUM_THREADS': [str(a) for a in [10,20,40,60,80,100,120,140,160]],
+            'OMP_PROC_BIND': ['close', 'spread'],
+            'OMP_SCHEDULE': [ a+','+str(b) for a in ['static', 'guided', 'dynamic'] for b in [1,8,32,64,128,256,512] ],
+        },
+        'jobsystem' : {
+            'joblaunch' : ''
+        }
     }
 }
 
@@ -22,43 +28,68 @@ machines = {
 # modified lulesh to do 200 iterations instead of 1000
 # will need to change region-execs to match correct number of region executions
 progs = {
-    'nas_bt': {
+    'bt_nas': {
         'xtime-regex':r'(?<=Time in seconds =)(\s*\d*\.\d*)(?=\s)',
-        'medprob': {
-            'exe': 'bt.C.x',
-        },
-        'lrgprob': {
-            'exe': 'bt.D.x',
+        'exe': {
+            'smlprob': 'bt.B.x',
+            'medprob': 'bt.C.x',
+            'lrgprob': 'bt.D.x'
+        }
+    },
+    'cg_nas': {
+        'xtime-regex':r'(?<=Time in seconds =)(\s*\d*\.\d*)(?=\s)',
+        'exe': {
+            'smlprob': 'cg.B.x',
+            'medprob': 'cg.C.x',
+            'lrgprob': 'cg.D.x'
+        }
+    },
+    'ft_nas': {
+        'xtime-regex':r'(?<=Time in seconds =)(\s*\d*\.\d*)(?=\s)',
+        'exe': {
+            'smlprob': 'ft.B.x',
+            'medprob': 'ft.C.x',
+            'lrgprob': 'ft.D.x'
+        }
+    },
+    'lu_nas': {
+        'xtime-regex':r'(?<=Time in seconds =)(\s*\d*\.\d*)(?=\s)',
+        'exe': {
+            'smlprob': 'lu.B.x',
+            'medprob': 'lu.C.x',
+            'lrgprob': 'lu.D.x'
+        }
+    },
+    'bfs_rodinia': {
+        'xtime-regex':r'(?<=Compute time: )(\s*\d*\.\d*)(?=\s)',
+        'exe': {
+            'smlprob': 'bfs 1 ../inputs/graph4096.txt',
+            'medprob': 'bfs 1 ../inputs/graph65536.txt',
+            'lrgprob': 'bfs 1 ../inputs/graph1MW_6.txt'
+        }
+    },
+    'cfd_rodinia': {
+        'xtime-regex':r'(?<=Compute time: )(\s*\d*\.\d*)(?=\s)',
+        'exe': {
+            'smlprob': 'euler3d_cpu ../inputs/fvcorr.domn.097K',
+            'medprob': 'euler3d_cpu ../inputs/missile.domn.0.2M',
+            'lrgprob': 'euler3d_cpu ../inputs/missile.domn.0.4M'
+        }
+    },
+    'hpcg': {
+        'xtime-regex':r'(?<=Compute time: )(\s*\d*\.\d*)(?=\s)',
+        'exe': {
+            'smlprob': 'euler3d_cpu ../inputs/fvcorr.domn.097K',
+            'medprob': 'euler3d_cpu ../inputs/missile.domn.0.2M',
+            'lrgprob': 'euler3d_cpu ../inputs/missile.domn.0.4M'
         }
     },
     'lulesh': {
         'xtime-regex': r'(?<=\()\s*(\d*\.\d*)(?= overall\))',
-        'smallprob': {
-            'exe': 'lulesh2.0 -s 30 -r 100 -b 0 -c 8 -i 200',
-        },
-        'medprob': {
-            'exe': 'lulesh2.0 -s 55 -r 100 -b 0 -c 8 -i 200',
-        },
-        'lrgprob': {
-            'exe': 'lulesh2.0 -s 80 -r 100 -b 0 -c 8 -i 200',
+        'exe':{
+            'smlprob': 'lulesh2.0 -s 30 -r 100 -b 0 -c 8 -i 200',
+            'medprob': 'lulesh2.0 -s 55 -r 100 -b 0 -c 8 -i 200',
+            'lrgprob': 'lulesh2.0 -s 80 -r 100 -b 0 -c 8 -i 200',
         }
     },
-    'nas_ft': {
-        'xtime-regex':r'(?<=Time in seconds =)(\s*\d*\.\d*)(?=\s)',
-        'medprob': {
-            'exe': 'ft.C.x',
-        },
-        'lrgprob': {
-            'exe': 'ft.D.x',
-        }
-    },
-    'nas_cg': {
-        'xtime-regex':r'(?<=Time in seconds =)(\s*\d*\.\d*)(?=\s)',
-        'medprob': {
-            'exe': 'cg.C.x',
-        },
-        'lrgprob': {
-            'exe': 'cg.D.x',
-        }
-    }
 }
